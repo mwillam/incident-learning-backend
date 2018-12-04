778518ecc4bc901888acb43f159181ffad8251df
mwillam <marvin.willam@onlinehome.de>
2018-12-04 15:08:27 +0100

    Add dynamic GET endpoint

--- src/Controller/ReportController.php
+++ src/Controller/ReportController.php
@@ -3,9 +3,11 @@
 namespace App\Controller;
 
 
+use App\Entity\Report;
 use App\Response\JsonApiNoContentResponse;
 use App\Response\JsonApiResponse;
 use App\Serializer\Serializer;
+use App\Service\RequestAnalyzer;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -16,11 +18,16 @@ class ReportController extends Controller
 {
     private $serializer;
     private $entityManager;
+    private $requestAnalyzer;
 
-    public function __construct(Serializer $serializer, EntityManagerInterface $entityManager)
-    {
+    public function __construct(
+        Serializer $serializer,
+        EntityManagerInterface $entityManager,
+        RequestAnalyzer $requestAnalyzer
+    ){
         $this->serializer = $serializer;
         $this->entityManager = $entityManager;
+        $this->requestAnalyzer = $requestAnalyzer;
     }
 
     /**
@@ -36,8 +43,10 @@ class ReportController extends Controller
             throw new \Exception("No content for creating a report.",401);
         }
 
+        $report = $this->serializer->deserialize($content);
+        $report->setCreatedNow();
+
         try {
-            $report = $this->serializer->deserialize($content);
             $this->entityManager->persist($report);
             $this->entityManager->flush();
         }
@@ -54,8 +63,18 @@ class ReportController extends Controller
      */
     public function getReport(Request $request)
     {
+        $queryKeys = $this->requestAnalyzer->filterEntityAttributes(Report::class, $request);
 
-        return new JsonApiNoContentResponse();
+        $queryParameters = $this->requestAnalyzer->filterQueryParameters(Report::class, $request);
+
+        $reports = $this->entityManager->getRepository(Report::class)
+            ->findByAttributes($queryKeys, $queryParameters);
+
+        if (empty($reports)) {
+            return new JsonApiResponse(array());
+        }
+
+        return new JsonApiResponse($reports);
     }
 
 }
